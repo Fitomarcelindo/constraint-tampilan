3e38c37dd5038366f441612d7aaa5bcba98c8448
fito <fitomarcelindo602@gmail.com>
2023-10-27 17:01:48 +0700

    update terbaru

--- app/src/main/java/by/marcel/tugas01/Main2.kt
+++ app/src/main/java/by/marcel/tugas01/Main2.kt
@@ -1,20 +1,44 @@
 package by.marcel.tugas01
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.Button
 import android.widget.TextView
 
 
+
 class Main2 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main2)
 
         val hsl = intent.getStringExtra(EXTRA_MESSAGE)
+        val hslgroup = intent.getStringExtra("GENDER")
+        val cek = intent.getStringExtra("HOBBIES")
 
-        val tvhasil = findViewById<TextView>(R.id.etUsername).apply { text = hsl }
-        tvhasil.text = ""+hsl
+        val tvhasil = findViewById<TextView>(R.id.etUsername)
+        tvhasil.text = hsl
+
+        val group = findViewById<TextView>(R.id.gender)
+        group.text = hslgroup
+
+        val hslcek = findViewById<TextView>(R.id.etPassword)
+        hslcek.text = cek
+
+        val btn = findViewById<Button>(R.id.btnshare)
+        btn.setOnClickListener{
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, "hey bloody ")
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent, "please choose apps"))
+        }
+
     }
 }
 
--- app/src/main/java/by/marcel/tugas01/MainActivity.kt
+++ app/src/main/java/by/marcel/tugas01/MainActivity.kt
@@ -10,15 +10,18 @@ import android.widget.EditText
 import android.view.View
 import android.widget.CheckBox
 import android.widget.RadioButton
+import android.widget.RadioGroup
 
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
+    private var jk: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val txt = findViewById<EditText>(R.id.nama)
+        val radio = findViewById<RadioGroup>(R.id.radioGroup)
         val pria = findViewById<RadioButton>(R.id.radioPria)
         val wanita = findViewById<RadioButton>(R.id.radioWanita)
         val cek1 = findViewById<CheckBox>(R.id.cekbox1)
@@ -26,42 +29,55 @@ class MainActivity : AppCompatActivity() {
         val cek3 = findViewById<CheckBox>(R.id.cekbox3)
         val btn = findViewById<Button>(R.id.btnlogin)
 
-        btn.setOnClickListener{
-            val marcel = txt.text.toString()
+        radio.setOnCheckedChangeListener { _, checkedid ->
+            when (checkedid) {
+                R.id.radioPria -> jk = "Pria"
+                R.id.radioWanita -> jk = "wanita"
+            }
 
-            val bymarcel = Intent(this@MainActivity,Main2::class.java).apply { putExtra(EXTRA_MESSAGE, marcel) }
+            btn.setOnClickListener {
+                val marcel = txt.text.toString()
+                val hobi = StringBuilder()
+                if (cek1.isChecked) hobi.append("Coding, ")
+                if (cek2.isChecked) hobi.append("Reading, ")
+                if (cek3.isChecked) hobi.append("Traveling, ")
+                val radioValue =
+                    if (pria.isChecked) "Pria" else if (wanita.isChecked) "Wanita" else ""
 
-            val intent = Intent(this@MainActivity,Main2::class.java)
-            startActivity(bymarcel)
-            
+                val bymarcel = Intent(this@MainActivity, Main2::class.java).apply {
+                    putExtra(EXTRA_MESSAGE, marcel)
+                    putExtra("HOBBIES", hobi.toString())
+                    putExtra("GENDER", radioValue)
+                }
+                startActivity(bymarcel)
+            }
         }
-
-    }
 //
 //    public final fun showdata(view: View) {
 //        val toast = Toast.makeText(applicationContext, "Data sudah ditambahkan", Toast.LENGTH_SHORT)
 //        toast.show()
 //    }
 
-    fun clear(view: View) {
-        val EditText = findViewById<EditText>(R.id.nama)
+        fun clear(view: View) {
+            val EditText = findViewById<EditText>(R.id.nama)
 
-        val radioPria = findViewById<RadioButton>(R.id.radioPria)
-        val radioWanita = findViewById<RadioButton>(R.id.radioWanita)
-        val cekbox1 = findViewById<CheckBox>(R.id.cekbox1)
-        val cekbox2 = findViewById<CheckBox>(R.id.cekbox2)
-        val cekbox3 = findViewById<CheckBox>(R.id.cekbox3)
+            val radioPria = findViewById<RadioButton>(R.id.radioPria)
+            val radioWanita = findViewById<RadioButton>(R.id.radioWanita)
+            val cekbox1 = findViewById<CheckBox>(R.id.cekbox1)
+            val cekbox2 = findViewById<CheckBox>(R.id.cekbox2)
+            val cekbox3 = findViewById<CheckBox>(R.id.cekbox3)
 
 
-        EditText.text.clear()
-        radioPria.isChecked = false
-        radioWanita.isChecked = false
-        cekbox1.isChecked = false
-        cekbox2.isChecked = false
-        cekbox3.isChecked = false
+            EditText.text.clear()
+            radioPria.isChecked = false
+            radioWanita.isChecked = false
+            cekbox1.isChecked = false
+            cekbox2.isChecked = false
+            cekbox3.isChecked = false
+
+        }
 
     }
-
 }
 
 
